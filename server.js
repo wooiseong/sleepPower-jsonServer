c238a6391d1e52d68c5328085662340fc9716dbd
Xiong <wooiseongseong96@gmail.com>
2024-12-14 14:18:56 +0800

    ver14

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('index.json');
+const router = jsonServer.router('db.json');
 
 const middlewares = jsonServer.defaults();
 
