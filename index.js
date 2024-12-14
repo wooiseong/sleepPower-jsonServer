e87ab3958c16c70dd298ff354d487391e102136b
Xiong <wooiseongseong96@gmail.com>
2024-12-14 14:28:26 +0800

    ver15

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
-  console.log('JSON Server is running on http://localhost:3000');
-})
+server.listen(port);
 
-module.exports = server;  
